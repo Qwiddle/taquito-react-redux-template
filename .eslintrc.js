037a995b46de391848171e5b71da576949310b64
Andrew Rubenstein <andjruben@gmail.com>
2023-01-16 20:25:29 -0500

    setup eslint
    using eslint airbnb config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  extends: ['airbnb'],
+  plugins: [],
+  rules: {
+    'react/prop-types': 1,
+  },
+};
