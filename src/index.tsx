0ebbdb377b98ff5bd66d1bc7ea87a91b3995e7be
Andrew Rubenstein <andjruben@gmail.com>
2023-01-16 20:24:49 -0500

    setup redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { store } from './store/configureStore';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
