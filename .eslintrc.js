11901bb895697a8d1c46598c322fdb7b0f87de57
Andrew Rubenstein <andjruben@gmail.com>
2023-01-21 00:05:16 -0500

    last fix for linting setup :)

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  ignorePatterns: ['config-overrides.js', 'eslintrc.js'],
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier', 'react', 'react-hooks'],
+  rules: {
+    'react/prop-types': 1,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+      {
+        usePrettierrc: true,
+      },
+    ],
+  },
+  parserOptions: {
+    project: 'tsconfig.json',
+  },
+};
